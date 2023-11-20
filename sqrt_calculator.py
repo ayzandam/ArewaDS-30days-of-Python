46e66ceff7fac9f0664b690bc90df25ec0b6ebe6
ayzandam <ayzandam95@gmail.com>
2023-11-20 23:30:09 +0100

    Initial Commit

---
+++ sqrt_calculator.py
@@ -0,0 +1,9 @@
+import math
+# Get an intger from a Keyboard
+num = int(input("Enter an Integer: "))
+
+# Find t
+# he square root of the number
+sqrt = math.sqrt(num)
+
+print(f"Square root of {num} is {sqrt}")
